667c3ae49e0641106f1e7d8541a34ee425315867
Dominik Kiss <dominik.kiss@easytechnologies.dev>
2023-02-01 20:25:12 +0100

    Edit reg form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,72 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="page_title_section">
+        <div class="page_title_overlay"></div>
+        <div class="page_header">
+            <div class="container">
+                <div class="row">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+
+                        <h1> regisztráció</h1>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+                        <div class="sub_title_section">
+                            <ul class="sub_title">
+                                <li><a href="#"> Kezdőlap &nbsp; / &nbsp;</a></li>
+                                <li>fiók</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="booking_table_wrapper dm_cover">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1 col-md-12 col-sm-12">
+                <div class="dmx_heading_wraper">
+                    <img src="assets/images/head3.png" alt="img">
+                    <h2>regisztráció</h2>
+                    <div class="bars bars2">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                    </div>
+
+                    <p>Regisztrálj a további funkciók használatához.</p>
+                </div>
+            </div>
+            <div class="col-lg-10 offset-lg-1 col-md-12 col-sm-12 col-12">
+                <div class="booking_form_field" style="text-transform: none;">
+                    <form method="POST" action="{{ route('darknine.app.auth.register') }}">
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                        <div id="logreg-forms">
+                            <form class="form-signin" style="text-transform: none">
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="text-danger" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                                <input type="text" id="inputName" name="name" class="form-control" placeholder="Teljes név" required="" autofocus="">
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="text-danger" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email cím" required="" autofocus="">
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="text-danger" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
+                                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Jelszó" required="">
+                                <input type="password" id="inputPassword" name="password_confirmation" class="form-control" placeholder="Jelszó megerősítése" required="">
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                                <button class="btn btn-success btn-block" type="submit"><i class="fas fa-sign-in-alt"></i> Regisztráció</button>
+                                <a href="{{ route('darknine.app.auth.showLoginForm') }}" id="forgot_pswd">Inkább bejelentkeznél?</a>
+                                <hr>
+                            </form>
                         </div>
                     </form>
                 </div>
@@ -74,4 +84,6 @@
         </div>
     </div>
 </div>
+
+<div class="row" style="padding-bottom: 200px;"></div>
 @endsection
